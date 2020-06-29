9e5633c7e4b6b1f41dab3f3774ccde5d67466a85
hj <137030104@qq.com>
2020-06-29 15:55:52 +0800

    cccc

--- Totals/src/com/chanpter2/API/MultiCllection.java
+++ Totals/src/com/chanpter2/API/MultiCllection.java
@@ -1,5 +1,5 @@
 package com.chanpter2.API;
-//hello world
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -13,7 +13,7 @@ import java.util.TreeSet;
 import java.util.Vector;
 import java.util.function.Function;
 import java.util.function.Supplier;
-//abc
+//abcdefg
 /**
  * collection接口 单列表集合
  * 子接口：
